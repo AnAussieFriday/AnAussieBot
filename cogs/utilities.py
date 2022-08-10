3ac63f594a9ad45094fd8be7daa4a9164d3b3dcc
AnAussieFriday <107454539+AnAussieFriday@users.noreply.github.com>
2022-08-10 21:26:28 +0400

    added 3 commands, a translate, detect, and galactic command

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -2,11 +2,16 @@ import discord
 from discord.ext import commands
 import requests
 from urllib.parse import quote_plus
-
+from googletrans import Translator
+import googletrans
+import math
+import aussielang
 
 class Utilities(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    translator = Translator()
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -93,22 +98,110 @@ class Utilities(commands.Cog):
     async def mcskin(self, ctx, type, username=None):
         mc_website = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
         name = mc_website.json()["id"]
-        list = ["skin","body","head","avatar","avatars"]
-        if type == "avatar" or type == "avatars":
-            await ctx.send(f'https://crafatar.com/avatars/{name}.png')
-        elif type == "head":
-            await ctx.send(f'https://crafatar.com/renders/head/{name}.png')
-        if type == "body":
-            await ctx.send(f'https://crafatar.com/renders/body/{name}.png')
-        if type == "skin":
-            await ctx.send(f'https://crafatar.com/skins/{name}.png')
-        elif type != list and username == None:
-            mc_website = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{type}")
-            name = mc_website.json()["id"]
-            await ctx.send(f'https://crafatar.com/renders/body/{name}.png')
-        else:
-            pass
+        try:
+            list = ["skin","body","head","avatar","avatars"]
+            if type == "avatar" or type == "avatars":
+                await ctx.send(f'https://crafatar.com/avatars/{name}.png?overlay=true')
+            elif type == "head":
+                await ctx.send(f'https://crafatar.com/renders/head/{name}.png?overlay=true')
+            if type == "body":
+                await ctx.send(f'https://crafatar.com/renders/body/{name}.png?overlay=true')
+            if type == "skin":
+                await ctx.send(f'https://crafatar.com/skins/{name}.png?overlay=true')
+            elif type != list and username == None:
+                mc_website = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{type}?overlay=true")
+                name = mc_website.json()["id"]
+                await ctx.send(f'https://crafatar.com/renders/body/{name}.png?overlay=true')
+            else:
+                pass
+        except:
+            embed = discord.Embed(title=f"An Error Occured",description="That MC user does not exist")
+            await ctx.send(embed=embed)
 
+    @commands.command(
+        aliases=["detect"],
+        name="language",
+        description="Detects the probability of the language the the sentence is in",
+    )
+    async def detect_lang(self, ctx, *, sentence):
+        bob = self.translator.detect(sentence)
+        bob2 = aussielang.languages_detect.get(bob.lang)
+        bob3 = math.ceil(bob.confidence / 0.01) * 0.01
+        await ctx.send(f"**Language:** {bob2}\n**Confidence:** {bob3}")
+
+    @commands.command(
+        name="translate",
+        description="Translate a message"
+    )
+    async def translate_lang(self, ctx, sentence, *, language="english"):
+        bob2 = aussielang.languages_translate.get(language)
+        bob = self.translator.translate(sentence,dest=bob2)
+        await ctx.send(f"**Translated from:** {sentence}\n**Translated to:** {bob.text}")
+
+    @commands.command(
+        description = "Translate from english to galactic or the other way around"
+    )
+    async def galactic(self, ctx, translate_mode,*, sentence: str):
+        if translate_mode == "encode" or translate_mode == "encrypt":
+            a = sentence.replace("a",'ᔑ')
+            b = a.replace("b",'ʖ')
+            c = b.replace("c","ᓵ")
+            d = c.replace("d","↸")
+            e = d.replace("e","ᒷ")
+            f = e.replace("f","⋮")
+            g = f.replace("g","⊣")
+            h = g.replace("h","⍑")
+            i = h.replace("i","╎")
+            j = i.replace("j","⋮")
+            k = j.replace("k","ꖌ")
+            l = k.replace("l","ꖎ")
+            m = l.replace("m","ᒲ")
+            n = m.replace("n","リ")
+            o = n.replace("o","𝙹")
+            p = o.replace("p","!")
+            q = p.replace("q","¡")
+            r = q.replace("r","ᑑ")
+            s = r.replace("s","∷")
+            t = s.replace("t","ᓭ")
+            u = t.replace("u","ℸ")
+            v = u.replace("v","⍊")
+            w = v.replace("w","∴")
+            x = w.replace("x","̇/")
+            y = x.replace("y","||")
+            z = y.replace("z","⨅")
+            z2 = z.replace(" "," ")
+            await ctx.send(z2)
+        elif translate_mode == "decode" or translate_mode == "decrypt":
+            a = sentence.replace("ᔑ",'a')
+            b = a.replace("ʖ",'b')
+            c = b.replace("ᓵ","c")
+            d = c.replace("↸","d")
+            e = d.replace("ᒷ","e")
+            f = e.replace("⋮","f")
+            g = f.replace("⊣","g")
+            h = g.replace("⍑","h")
+            i = h.replace("╎","i")
+            j = i.replace("⋮","j")
+            k = j.replace("ꖌ","k")
+            l = k.replace("ꖎ","l")
+            m = l.replace("ᒲ","m")
+            n = m.replace("リ","n")
+            o = n.replace("𝙹","o")
+            p = o.replace("!","p")
+            q = p.replace("¡","q")
+            r = q.replace("ᑑ","r")
+            s = r.replace("∷","s")
+            t = s.replace("ᓭ","t")
+            u = t.replace("ℸ","u")
+            v = u.replace("⍊","v")
+            w = v.replace("∴","w")
+            x = w.replace("̇/","x")
+            y = x.replace("||","y")
+            z = y.replace("⨅","z")
+            z2 = z.replace(" "," ")
+            await ctx.send(z2)
+        else:
+            await ctx.send("That is not a valid mode! Use either `decrypt`, `encrypt`, `decode`, or `encode`")
 
 async def setup(bot):
     await bot.add_cog(Utilities(bot))
